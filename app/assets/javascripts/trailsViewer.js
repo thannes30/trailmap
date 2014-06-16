0abf4e379968ca3e85eabd0189df780cea2848a6
thannes30 <thannes30@yahoo.com>
2014-06-16 11:50:54 -0400

    added content to info window on hover

--- app/assets/javascripts/trailsViewer.js
+++ app/assets/javascripts/trailsViewer.js
@@ -18,11 +18,8 @@ function initialize() {
   allTrails.fetch(function(){
     // callback to fetch after all models are added
     $.each(allTrails.trails, function(prop, val){
-      // console.log(prop, val)
-
       var title = title;
-      var comments = comments;
-
+      var description = description;
       var hikeCoords = new google.maps.LatLng(val.startMarker[0], val.startMarker[1]);
       marker = new google.maps.Marker({
         map:map,
